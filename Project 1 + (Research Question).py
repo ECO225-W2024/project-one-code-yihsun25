9aa0402db47c29fd45ac55138efd77e527677622
Yi Hsun Wang <aliceee.wang@mail.utoronto.ca>
2024-01-28 17:06:59 -0500

    testing

---
+++ Project 1 + (Research Question).py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Research Question 
+# 
+# ## Introduction 
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('AB_NYC_2019.csv')
+
+
+# ## Data Cleaning/Loading
+
+# In[ ]:
+
+
+
+
+
+# ## Summary Statistics Table
+
+# In[4]:
+
+
+df.describe()
+
+
+# In[3]:
+
+
+df.describe().head()
+
+
+# ## Plots, Histograms, Figures 
+
+# In[ ]:
+
+
+
+
+
+# ## Conclusion
+
+# In[ ]:
+
+
+
+
